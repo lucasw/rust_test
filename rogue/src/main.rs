fb1d63385fe6f8a0e63fb78b33908b447dd3c8a9
Lucas Walter <wsacul@gmail.com>
2020-09-05 18:59:16 -0700

    Using Vec for map

--- rogue/src/main.rs
+++ rogue/src/main.rs
@@ -23,30 +23,55 @@ impl Default for Cell {
             large_object: std::ptr::null_mut(),
         }
     }
+}
 
+struct Map {
+    _width: usize,
+    _height: usize,
+    cells: Vec<Cell>,
+    // Box<[Cell]>
+}
+
+impl Map {
+    // fn default(width: usize, height: usize) -> Map {
+    fn new(width: usize, height: usize) -> Map {
+        Map {
+            _width: width,
+            _height: height,
+            cells: vec![Cell::default(); width * height],
+        }
+    }
+
+    fn get(&self, x: usize, y: usize) -> Cell {
+        return self.cells[y * self._width + x];
+    }
 }
 
 fn main() {
     println!("foo foo");
 
+    /*
     let xs: &[i32] = &[1, 2, 3];
-
-    let _player: *mut Player;  // = &Player::default();
-    _player = &mut Player::default();
-
     print!("F");
     println!("{:?}", xs);
+    */
+    /*
+    let _player: *mut Player;  // = &Player::default();
+    _player = &mut Player::default();
+    */
 
     const WIDTH: usize = 80;
     const HEIGHT: usize = 30;
-    let mut row: [Cell; WIDTH] = [Cell::default(); WIDTH];
+    let mut _map = Map::new(WIDTH, HEIGHT);
+    // let mut row: [Cell; WIDTH] = [Cell::default(); WIDTH];
 
-    row[37].large_object = _player;
+    // row[37].large_object = _player;
 
     // println!("{:#?}", row);
     print!("\n");
-    for _ in 0..HEIGHT {
-        for element in row.iter() {
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            let element = _map.get(x, y);
             if element.large_object.is_null() {
                 print!("{}", element.floor);
             } else {
