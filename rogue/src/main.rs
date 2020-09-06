802a5f07695dbe57ed47e17e4468ae07eeba8414
Lucas Walter <wsacul@gmail.com>
2020-09-05 19:50:56 -0700

    Trying out keyboard input

--- rogue/src/main.rs
+++ rogue/src/main.rs
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2020 by Lucas Walter
+ * September 2020
+ * Learn rust with a weakly roguelike
+ *
+ */
+
+use device_query::{DeviceQuery, Keycode};
+
 struct Player {
     symbol: char,
 }
@@ -21,6 +30,18 @@ impl Default for Cell {
         Cell {
             floor: '.',
             large_object: std::ptr::null_mut(),
+        }
+    }
+}
+
+impl Cell {
+    fn print(&self) {
+        if self.large_object.is_null() {
+            print!("{}", self.floor);
+        } else {
+            unsafe {
+                print!("{}", (*self.large_object).symbol);
+            }
         }
     }
 }
@@ -49,6 +70,28 @@ impl Map {
     fn put(&mut self, _player: *mut Player, x: usize, y: usize) {
         self.cells[y * self._width + x].large_object = _player;
     }
+
+    fn move_object(&mut self, old_x: usize, old_y: usize, new_x: usize, new_y: usize) {
+        if old_x == new_x && old_y == new_y {
+            return;
+        }
+        let old_ind : usize = old_y * self._width + old_x;
+        let new_ind : usize = new_y * self._width + new_x;
+        self.cells[new_ind].large_object = self.cells[old_ind].large_object;
+        self.cells[old_ind].large_object = std::ptr::null_mut();
+    }
+
+    fn print(&self) {
+        print!("\n");
+        for y in 0..self._height {
+            for x in 0..self._width {
+                // let element = self.get(x, y);
+                self.get(x, y).print();
+            }
+            print!("\n");
+        }
+        print!("\n");
+    }
 }
 
 fn main() {
@@ -66,22 +109,44 @@ fn main() {
     const HEIGHT: usize = 30;
     let mut _map = Map::new(WIDTH, HEIGHT);
 
-    _map.put(_player, 40, 25);
+    let mut px : usize = 40;
+    let mut py : usize = 25;
+    _map.put(_player, px, py);
 
-    // println!("{:#?}", row);
-    print!("\n");
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            let element = _map.get(x, y);
-            if element.large_object.is_null() {
-                print!("{}", element.floor);
-            } else {
-                unsafe {
-                    print!("{}", (*element.large_object).symbol);
-                }
+    let device_state = device_query::DeviceState::new();
+
+    let mut count: i32 = 0;
+    loop {
+        println!("{}", count);
+        _map.print();
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        let keys: Vec<Keycode> = device_state.get_keys();
+
+        let old_px = px;
+        let old_py = py;
+        if keys.contains(&Keycode::H) {
+            if px > 0 {
+                px -= 1;
             }
         }
-        print!("\n");
+        if keys.contains(&Keycode::L) {
+            if px < _map._width - 1 {
+                px += 1;
+            }
+        }
+        if keys.contains(&Keycode::K) {
+            if py > 0 {
+                py -= 1;
+            }
+        }
+        if keys.contains(&Keycode::J) {
+            if py < _map._height - 1 {
+                py += 1;
+            }
+        }
+        println!("");
+        _map.move_object(old_px, old_py, px, py);
+        count += 1;
     }
-    print!("\n");
+    // println!("{:#?}", row);
 }
