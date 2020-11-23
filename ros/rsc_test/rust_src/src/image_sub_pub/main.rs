2c4d9b85a3d66bce3c07da87f8cff3fff5c3e28e
Lucas Walter <wsacul@gmail.com>
2020-11-23 12:03:20 -0800

    Subscribe to an image and print out msg fields, but not manipulating the data yet.

---
+++ ros/rsc_test/rust_src/src/image_sub_pub/main.rs
@@ -0,0 +1,49 @@
+// Subscribe to an image topic and publish a modified version of the image in response.
+// For now modify the image directly in the callback and publish that output,
+// but later maybe store the image and then modify it in a separate thread.
+//
+
+use rosrust_msg::sensor_msgs::Image;
+use rosrust::api::raii as ros;
+// use std::sync::Mutex;
+
+struct ImageSubPub {
+    _image_sub: ros::Subscriber,  // <Image>
+    // image_pub: ros::Publisher<Image>,
+    // TODO(lucasw) add a variable float to scale the incoming image with
+    // fr : f32,
+}
+
+
+impl ImageSubPub {
+    fn new() -> Self{
+        rosrust::ros_info!("new ImageSubPub");
+        let image_pub = rosrust::publish("image_out", 4).unwrap();
+        let fr = 2.5;
+
+        let image_callback = {
+            move |msg: Image| {
+                rosrust::ros_info!("preferred callback {} {} {} {} {}",
+                                   msg.width, msg.height, msg.data.len(), msg.encoding,
+                                   fr);
+                image_pub.send(msg).unwrap();
+            }
+        };
+
+        let _image_sub = rosrust::subscribe("image_in", 4, image_callback).unwrap();
+
+        let image_sub_pub = Self {
+            _image_sub,
+            // image_pub,
+            // fr,
+        };
+
+        image_sub_pub
+    }
+}
+
+fn main() {
+    rosrust::init("image_sub_pub_rs");
+    let _image_sub_pub = ImageSubPub::new();
+    rosrust::spin();
+}
---
+++ ros/rsc_test/src/image_sub_pub.cpp
@@ -0,0 +1,2 @@
+// Subscribe to an image topic and publish a modified version of the image in response.
+// For now modify the image directly in the callback and publish that output.
