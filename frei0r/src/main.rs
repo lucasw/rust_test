e579fbe58bf60ba3cb3b496d3f158d220d26ee4b
Lucas Walter <wsacul@gmail.com>
2020-11-25 10:01:06 -0800

    Now getting strings out of pointers, not sure if they need to be freed somewhere still?

--- frei0r/src/main.rs
+++ frei0r/src/main.rs
@@ -2,7 +2,9 @@ extern crate libloading;
 
 // use std::env;
 use libloading::{Library, Symbol};
-use std::ffi::CString;
+// use std::ffi::CString;
+use std::ffi::CStr;
+use std::ptr;
 
 // type AddFunc = fn(isize, isize) -> isize;
 
@@ -108,6 +110,60 @@ extern "C" {
 */
 pub type F0rGetPluginInfo = fn(*mut F0rPluginInfo);
 
+pub struct F0rWrapper {
+    raw_info: F0rPluginInfo,
+    name: String,
+    author: String,
+    explanation: String,
+}
+
+impl F0rWrapper {
+    fn print(&self) {
+        // TODO(lucasw) only want to call from_raw once needs to be i
+        println!("'{}' '{}'", self.name, self.author);
+        println!("'{}'", self.explanation);
+        println!("plugin type {}, color model {}",
+                 self.raw_info.plugin_type, self.raw_info.color_model);
+        println!("frei0r version {} {} {}",
+                 self.raw_info.frei0r_version, self.raw_info.major_version, self.raw_info.minor_version);
+        println!("num params {}", self.raw_info.num_params);
+    }
+
+    fn default(lib: &Library) -> F0rWrapper {
+        println!("f0r_get_plugin_info");
+        let mut raw_info = F0rPluginInfo {
+            name: ptr::null(),
+            author: ptr::null(),
+            plugin_type: 0,
+            color_model: 0,
+            frei0r_version: 0,
+            major_version: 0,
+            minor_version: 0,
+            num_params: 0,
+            explanation: ptr::null(),
+        };
+
+        let mut name = String::from("foo2");
+        let mut author = String::from("bar2");
+        let mut explanation = String::from("none2");
+        unsafe {
+            let f0r_get_plugin_infor: Symbol<F0rGetPluginInfo> = lib.get(b"f0r_get_plugin_info").unwrap();
+            // Need to write a Default for this to work?
+            f0r_get_plugin_infor(&mut raw_info);
+
+            name = CStr::from_ptr(raw_info.name).to_string_lossy().into_owned();
+            author = CStr::from_ptr(raw_info.author).to_string_lossy().into_owned();
+            explanation = CStr::from_ptr(raw_info.explanation).to_string_lossy().into_owned();
+        }
+
+        F0rWrapper {
+            raw_info,
+            name,
+            author,
+            explanation,
+        }
+    }
+}
 
 fn main() {
     let library_path = "/usr/lib/frei0r-1/saturat0r.so";  // env::args().nth(1).expect("USAGE: loading <LIB>");
@@ -123,28 +179,15 @@ fn main() {
         let f0r_initor: Symbol<F0rInit> = lib.get(b"f0r_init").unwrap();
         f0r_initor();
 
-        println!("f0r_get_plugin_info");
-        let f0r_get_plugin_infor: Symbol<F0rGetPluginInfo> = lib.get(b"f0r_get_plugin_info").unwrap();
-        // Need to write a Default for this to work?
-        let mut info = F0rPluginInfo {
-            name: CString::new("abc").unwrap().as_ptr(),
-            author: CString::new("def").unwrap().as_ptr(),
-            plugin_type: 0,
-            color_model: 0,
-            frei0r_version: 0,
-            major_version: 0,
-            minor_version: 0,
-            num_params: 0,
-            explanation: CString::new("none").unwrap().as_ptr(),
-        };
-        f0r_get_plugin_infor(&mut info);
-        // Not sure about the strings yet
-        // println!("num_params {} {}", CString::from_raw(info.name).to_str().unwrap(), info.num_params);
-        println!("plugin type {}, color model {}",
-                 info.plugin_type, info.color_model);
-        println!("frei0r version {} {} {}",
-                 info.frei0r_version, info.major_version, info.minor_version);
-        println!("num params {}", info.num_params);
+        /*
+        let t0 = CString::new("abc").unwrap();
+        let t0_ptr = t0.into_raw();
+        let t1 = CString::from_raw(t0_ptr);
+        println!("cstring test {:?}", t1);
+        */
+
+        let f0r = F0rWrapper::default(&lib);
+        f0r.print();
 
         let f0r_constructor: Symbol<F0rConstruct> = lib.get(b"f0r_construct").unwrap();
         let _instance = f0r_constructor(width, height);
