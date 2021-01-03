bc02863980f13e4a89e04ea293c6f41acace3bd1
Lucas Walter <wsacul@gmail.com>
2021-01-03 15:38:56 -0800

    Show using a rust residual evaluation function, but have to use numeric diff instead of auto diff- unless the Jet type or a generic template that includes the Jet type can be used on the rust side.

--- ceres/ceres_cxx/src/main.rs
+++ ceres/ceres_cxx/src/main.rs
@@ -1,5 +1,11 @@
+use cxx::CxxVector;
+
 #[cxx::bridge(namespace = "org::ceres_example")]
 mod ffi {
+
+    extern "Rust" {
+        fn evaluate(val: f64) -> f64;
+    }
 
     // C++ types and signatures exposed to Rust.
     unsafe extern "C++" {
@@ -8,11 +14,22 @@ mod ffi {
         type CeresExample;
 
         fn new_ceres_example() -> UniquePtr<CeresExample>;
-        fn run(&self);
+
+        fn run(&self, vals: &Vec<f64>);
     }
+}
+
+pub fn evaluate(val: f64) -> f64 {
+    let residual = 12.3 - val;
+    residual
 }
 
 fn main() {
     let ceres_example = ffi::new_ceres_example();
-    ceres_example.run();
+    // is it possible to create a CxxVector on the rust side?  There isn't a CxxVector::new()
+    // let vals = CxxVector;  // ::<f64>();
+    let mut vals = Vec::new();
+    vals.push(5.342);
+    vals.push(8.0);
+    ceres_example.run(&vals);
 }
