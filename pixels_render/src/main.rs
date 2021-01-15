fb42488e9229abc8dbefa09bf3f3fcfe10afe285
Lucas Walter <wsacul@gmail.com>
2021-01-14 21:03:53 -0800

    Trying out pixels with winit for drawing to the screen, pixels seems fine but winit is very slow with keyboard handling, try out sdl2 instead.

---
+++ pixels_render/src/main.rs
@@ -0,0 +1,143 @@
+use pixels::{Error, Pixels, SurfaceTexture};
+use std::{thread, time};
+use winit::dpi::{LogicalPosition, LogicalSize, PhysicalSize};
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit_input_helper::WinitInputHelper;
+
+const SCREEN_WIDTH: u32 = 320;
+const SCREEN_HEIGHT: u32 = 200;
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let (window, p_width, p_height, mut _hidpi_factor) =
+        create_window("Pixels Render", &event_loop);
+    let surface_texture = SurfaceTexture::new(p_width, p_height, &window);
+    let mut pixels = Pixels::new(SCREEN_WIDTH, SCREEN_HEIGHT, surface_texture).unwrap();
+
+    let mut paused = false;
+
+    let mut frame_count: u32 = 0;
+
+    event_loop.run(move |event, _, control_flow| {
+        // The one and only event that winit_input_helper doesn't have for us...
+        if let Event::RedrawRequested(_) = event {
+
+            // draw the pixels
+            {
+                let screen = pixels.get_frame();
+                let mut count: u32 = frame_count;
+                for pix in screen.chunks_exact_mut(4) {
+                    let r: u8 = (count % 0xff) as u8;
+                    let g: u8 = ((count / 2) % 0xff) as u8;
+                    let b: u8 = ((count / 4) % 0xff) as u8;
+                    let color = [0, r, g, b];
+                    pix.copy_from_slice(&color);
+
+                    count += 1;
+                }
+                // frame_count += 1;
+            }
+
+            if pixels
+                .render()
+                .map_err(|e| eprintln!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            thread::sleep(time::Duration::from_millis(33));
+        }
+
+        // For everything else, for let winit_input_helper collect events to build its state.
+        // It returns `true` when it is time to update our game state and request a redraw.
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+            if input.key_pressed(VirtualKeyCode::P) {
+                paused = !paused;
+            }
+            if input.key_held(VirtualKeyCode::Left) {
+                frame_count -= 1;
+            }
+            if input.key_held(VirtualKeyCode::Right) {
+                frame_count += 1;
+            }
+
+            // Adjust high DPI factor
+            if let Some(factor) = input.scale_factor_changed() {
+                _hidpi_factor = factor;
+            }
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                pixels.resize(size.width, size.height);
+            }
+
+            window.request_redraw();
+        }
+    });
+}
+
+
+/// Create a window for the game.
+///
+/// Automatically scales the window to cover about 2/3 of the monitor height.
+///
+/// # Returns
+///
+/// Tuple of `(window, surface, width, height, hidpi_factor)`
+/// `width` and `height` are in `PhysicalSize` units.
+fn create_window(
+    title: &str,
+    event_loop: &EventLoop<()>,
+) -> (winit::window::Window, u32, u32, f64) {
+    // Create a hidden window so we can estimate a good default window size
+    let window = winit::window::WindowBuilder::new()
+        .with_visible(false)
+        .with_title(title)
+        .build(&event_loop)
+        .unwrap();
+    let hidpi_factor = window.scale_factor();
+
+    // Get dimensions
+    let width = SCREEN_WIDTH as f64;
+    let height = SCREEN_HEIGHT as f64;
+    let (monitor_width, monitor_height) = {
+        if let Some(monitor) = window.current_monitor() {
+            let size = monitor.size().to_logical(hidpi_factor);
+            (size.width, size.height)
+        } else {
+            (width, height)
+        }
+    };
+    let scale = (monitor_height / height * 2.0 / 3.0).round().max(1.0);
+
+    // Resize, center, and display the window
+    let min_size: winit::dpi::LogicalSize<f64> =
+        PhysicalSize::new(width, height).to_logical(hidpi_factor);
+    let default_size = LogicalSize::new(width * scale, height * scale);
+    let center = LogicalPosition::new(
+        (monitor_width - width * scale) / 2.0,
+        (monitor_height - height * scale) / 2.0,
+    );
+    window.set_inner_size(default_size);
+    window.set_min_inner_size(Some(min_size));
+    window.set_outer_position(center);
+    window.set_visible(true);
+
+    let size = default_size.to_physical::<f64>(hidpi_factor);
+
+    (
+        window,
+        size.width.round() as u32,
+        size.height.round() as u32,
+        hidpi_factor,
+    )
+}
+
