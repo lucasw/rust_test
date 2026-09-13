0022894b687e673593953a60fed28bba1a82159f
Lucas Walter <wsacul@gmail.com>
2026-09-13 08:53:12 -0700

    use egui_plot and add pause and zoom controls

--- texture_egui/src/main.rs
+++ texture_egui/src/main.rs
@@ -1,5 +1,6 @@
 use eframe::egui;
-// use egui_plot::{Plot, PlotImage};
+use egui_plot::{Plot, PlotImage, PlotPoint};
+use std::time::{Duration, Instant};
 
 struct Particle {
     x: f32,
@@ -11,7 +12,11 @@ impl Particle {
 }
 */
 
+#[derive(Default)]
 struct App {
+    width: usize,
+    height: usize,
+
     particles0: Vec<Particle>,
     /// particles will move to higher scoring neighbor cells here
     move_gradient0a: Vec<f32>,
@@ -24,15 +29,34 @@ struct App {
     max1: f32,
 
     color_image: egui::ColorImage,
-    // texture_handle: egui::TextureHandle,
+    texture_handle: Option<egui::TextureHandle>,
+
+    // use_plot_ui: bool,
+    allow_zoom: bool,
+    pause: bool,
+
+    instant: Option<Instant>,
+    update_elapsed: Duration,
     counter: usize,
 }
 
 impl eframe::App for App {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
-        self.update();
+        ui.horizontal(|ui| {
+            if !self.pause
+                && (self.instant.is_none()
+                    || self.instant.unwrap().elapsed()
+                        > Duration::from_millis((1000.0 * 1.0 / 35.0) as u64))
+            {
+                self.instant = Some(Instant::now());
+                self.update();
+                self.update_elapsed = self.instant.unwrap().elapsed();
+            }
+            ui.label(format!(
+                "update elapsed {:.3}ms",
+                self.update_elapsed.as_millis()
+            ));
 
-        egui::CentralPanel::default().show(ui, |ui| {
             let size = self.color_image.size;
             ui.label(format!(
                 "vec size {size:?} {} -> {}",
@@ -40,19 +64,69 @@ impl eframe::App for App {
                 self.color_image.pixels.len()
             ));
             ui.label(format!("max0 {:.3}, max1 {:.3}", self.max0, self.max1));
-            // TODO(lucasw) use egui_plot to scale the image to the window
-            let texture_handle = ui.ctx().load_texture(
-                "image",
-                self.color_image.clone(),
-                egui::TextureOptions::NEAREST,
-            );
-
-            ui.image(&texture_handle);
-
-            // TODO(lucasw) will this sometimes exceed 30 Hz if the mouse is moving?
-            ui.ctx()
-                .request_repaint_after(std::time::Duration::from_secs_f32(1.0 / 30.0));
         });
+
+        let mut reset_plot = false;
+        ui.horizontal(|ui| {
+            ui.checkbox(&mut self.pause, "pause");
+
+            // ui.checkbox(&mut self.use_plot_ui, "use egui_plot").changed();
+            reset_plot = ui
+                .checkbox(&mut self.allow_zoom, "allow zoom and drag")
+                .changed()
+                && !self.allow_zoom;
+
+            if ui.button("reset").clicked() {
+                self.reset();
+            }
+        });
+
+        let texture_handle = self.texture_handle.get_or_insert(ui.ctx().load_texture(
+            "image",
+            self.color_image.clone(),
+            egui::TextureOptions::NEAREST,
+        ));
+
+        // TODO(lucasw) use egui_plot to scale the image to the window
+        texture_handle.set(self.color_image.clone(), egui::TextureOptions::NEAREST);
+
+        // if self.use_plot_ui {
+        {
+            let mut plot = Plot::new("image")
+                // .grid_spacing(egui::Rangef::new(10000.0, 10000.0))
+                .show_grid([false, false])
+                .set_margin_fraction(egui::vec2(0.0, 0.0))
+                .auto_bounds(true)
+                .allow_double_click_reset(true)
+                .allow_zoom(self.allow_zoom)
+                .allow_drag(self.allow_zoom)
+                .allow_scroll(false)
+                .show_axes([false, false])
+                .data_aspect(1.0);
+            if reset_plot {
+                plot = plot.reset();
+            }
+
+            // egui::CentralPanel::default().show(ui, |ui| {
+            let _plot_response = plot.show(ui, |plot_ui| {
+                let size = texture_handle.size_vec2();
+                let plot_image = PlotImage::new(
+                    "image",
+                    texture_handle.id(),
+                    PlotPoint::new(size[0] / 2.0, -size[1] / 2.0),
+                    size,
+                );
+                plot_ui.image(plot_image);
+            });
+        }
+        /* else {
+            ui.image(texture_handle);
+        }
+        */
+
+        // TODO(lucasw) will this sometimes exceed 30 Hz if the mouse is moving?
+        ui.ctx()
+            .request_repaint_after(std::time::Duration::from_secs_f32(1.0 / 30.0));
     }
 }
 
@@ -135,15 +209,50 @@ fn move_particles(
 
         let ind = particle.y.round() as usize * width + particle.x.round() as usize;
         // if ind < field1.len() {
-            // attract the opposite particles
-            field1[ind] += 0.1;
-            // repel other particles (though this also self repels)
-            field0[ind] -= 0.05;
+        // attract the opposite particles
+        field1[ind] += 0.1;
+        // repel other particles (though this also self repels)
+        field0[ind] -= 0.05;
         // }
     });
 }
 
 impl App {
+    fn reset(&mut self) {
+        self.move_gradient0a = vec![0.0; self.width * self.height];
+        self.move_gradient1a = self.move_gradient0a.clone();
+        for yi in 0..self.height {
+            for xi in 0..self.width {
+                let ind = yi * self.width + xi;
+                let fr = 0.000004;
+                let sc = 0.001;
+                let base = 0.0001;
+                self.move_gradient0a[ind] = base + rand::random::<f32>() * sc + yi as f32 * fr;
+                self.move_gradient1a[ind] =
+                    base + rand::random::<f32>() * sc + (self.height - yi) as f32 * fr;
+            }
+        }
+        self.move_gradient0b = self.move_gradient0a.clone();
+        self.move_gradient1b = self.move_gradient1a.clone();
+
+        self.particles0.clear();
+        for _i in 0..250 {
+            let x = rand::random::<f32>() * (self.width - 1) as f32;
+            let y = 0.25 * (rand::random::<f32>() * (self.height - 1) as f32);
+            self.particles0.push(Particle { x, y });
+        }
+
+        self.particles1.clear();
+        for _i in 0..250 {
+            let x = rand::random::<f32>() * (self.width - 1) as f32;
+            let y = 0.25 * (rand::random::<f32>() * (self.height - 1) as f32);
+            self.particles1.push(Particle {
+                x,
+                y: self.height as f32 - y,
+            });
+        }
+    }
+
     fn update(&mut self) {
         let [width, height] = self.color_image.size;
 
@@ -276,7 +385,7 @@ impl App {
             let ind = (particle.y.round() * width as f32 + particle.x.round()) as usize;
             let color = egui::Color32::from_rgb(255, 130, 225);
             if ind < (width * height) {
-                self.color_image.pixels[ind as usize] = color;
+                self.color_image.pixels[ind] = color;
             }
         });
 
@@ -284,7 +393,7 @@ impl App {
             let ind = (particle.y.round() * width as f32 + particle.x.round()) as usize;
             let color = egui::Color32::from_rgb(190, 255, 130);
             if ind < (width * height) {
-                self.color_image.pixels[ind as usize] = color;
+                self.color_image.pixels[ind] = color;
             }
         });
 
@@ -293,9 +402,8 @@ impl App {
 }
 
 fn main() {
-    // let width = 640;
-    // let height = 480;
-    let (width, height) = (1280, 720);
+    let (width, height) = (640, 480);
+    // let (width, height) = (1280, 720);
 
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
@@ -304,47 +412,16 @@ fn main() {
     };
 
     let fill_color = egui::Color32::BLACK;
-
-    let mut move_gradient0 = vec![0.0; width * height];
-    let mut move_gradient1 = move_gradient0.clone();
-    for yi in 0..height {
-        for xi in 0..width {
-            let ind = yi * width + xi;
-            let fr = 0.000004;
-            let sc = 0.001;
-            let base = 0.0001;
-            move_gradient0[ind] = base + rand::random::<f32>() * sc + yi as f32 * fr;
-            move_gradient1[ind] = base + rand::random::<f32>() * sc + (height - yi) as f32 * fr;
-        }
-    }
+    let color_image = egui::ColorImage::filled([width, height], fill_color);
 
     let mut app = App {
-        particles0: Vec::new(),
-        move_gradient0a: move_gradient0.clone(),
-        move_gradient0b: move_gradient0,
-        max0: 0.0,
-        particles1: Vec::new(),
-        move_gradient1a: move_gradient1.clone(),
-        move_gradient1b: move_gradient1,
-        max1: 0.0,
-        color_image: egui::ColorImage::filled([width, height], fill_color),
-        counter: 0,
+        width,
+        height,
+        color_image,
+        ..Default::default()
     };
 
-    for _i in 0..250 {
-        let x = rand::random::<f32>() * (width - 1) as f32;
-        let y = 0.25 * (rand::random::<f32>() * (height - 1) as f32);
-        app.particles0.push(Particle { x, y });
-    }
-
-    for _i in 0..250 {
-        let x = rand::random::<f32>() * (width - 1) as f32;
-        let y = 0.25 * (rand::random::<f32>() * (height - 1) as f32);
-        app.particles1.push(Particle {
-            x,
-            y: height as f32 - y,
-        });
-    }
+    app.reset();
 
     let rv = eframe::run_native("texture_egui", options, Box::new(|_cc| Ok(Box::new(app))));
 
